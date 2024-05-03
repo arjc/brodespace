1b70fd06cf4b5f61f6fd811714f963a3631cdb55
Brode <76899772+brodespace@users.noreply.github.com>
2024-05-03 05:37:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const homeNavButton = document.querySelector("#homeNavButton");
+const projectsNavButton = document.querySelector("#projectsNavButton");
+const contactNavButton = document.querySelector("#contactNavButton");
+const brogoInteractive = document.querySelector("#interactiveBrogo img");
+
+setTimeout(() => {brogoInteractive.style.filter = "none"; brogoInteractive.style.boxShadow = "0 0 30px var(--colorSecondary)"}, 4000);
