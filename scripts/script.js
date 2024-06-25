88ba9544be506d9cc8f65c78492a09cac0fa16df
brodespace <arjunsarovaram@gmail.com>
2024-06-25 13:24:02 +0530

    modified js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,23 @@
 const homeNavButton = document.querySelector("#homeNavButton");
 const projectsNavButton = document.querySelector("#projectsNavButton");
 const contactNavButton = document.querySelector("#contactNavButton");
+
 const brogoInteractive = document.querySelector("#interactiveBrogo img");
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      console.log(entry);
+      if (entry.isIntersecting){
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
+      }
+  });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
 
 setTimeout(() => {
   brogoInteractive.style.filter = "none";
