ed18f04e82a045d7077a823deeb322e4a07b2bca
brodespace <arjunsarovaram@gmail.com>
2024-06-27 07:23:49 +0530

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -2,23 +2,22 @@ const homeNavButton = document.querySelector("#homeNavButton");
 const projectsNavButton = document.querySelector("#projectsNavButton");
 const contactNavButton = document.querySelector("#contactNavButton");
 
-const brogoInteractive = document.querySelector("#interactiveBrogo img");
-
 const hiddenElements = document.querySelectorAll('.hidden');
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-      console.log(entry);
-      if (entry.isIntersecting){
+    console.log(entry);
+    if (entry.isIntersecting){
           entry.target.classList.add('show');
-      } else {
+        } else {
           entry.target.classList.remove('show');
-      }
-  });
-});
-
-hiddenElements.forEach((el) => observer.observe(el));
-
+        }
+      });
+    });
+    
+    hiddenElements.forEach((el) => observer.observe(el));
+    
+const brogoInteractive = document.querySelector("#interactiveBrogo img");
 setTimeout(() => {
   brogoInteractive.style.filter = "none";
   brogoInteractive.style.boxShadow = "0 0 30px var(--colorSecondary)";
