c1a70f4feedba1e8c52575888aa30eba2d7876b7
brodespace <arjunsarovaram@gmail.com>
2024-06-27 20:30:34 +0530

    fixed errors in index.html. fixed overflow of #home (formerly known as #content)

--- scripts/script.js
+++ scripts/script.js
@@ -12,12 +12,29 @@ const observer = new IntersectionObserver((entries) => {
         entry.target.classList.add("show");
       }, 250);
     } else {
-        entry.target.classList.remove("show");
+      entry.target.classList.remove("show");
     }
   });
 });
 
 hiddenElements.forEach((el) => observer.observe(el));
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(".nav-element");
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove("active");
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+            });
+        };
+    });
+};
 
 const brogoInteractive = document.querySelector("#interactiveBrogo img");
 setTimeout(() => {
