64d156babedabfe9fdc5d0bb2aedf5611b3a6243
brodespace <arjunsarovaram@gmail.com>
2024-06-27 09:27:28 +0530

    lazy loading

--- scripts/script.js
+++ scripts/script.js
@@ -2,21 +2,23 @@ const homeNavButton = document.querySelector("#homeNavButton");
 const projectsNavButton = document.querySelector("#projectsNavButton");
 const contactNavButton = document.querySelector("#contactNavButton");
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
-    if (entry.isIntersecting){
-          entry.target.classList.add('show');
-        } else {
-          entry.target.classList.remove('show');
-        }
-      });
-    });
-    
-    hiddenElements.forEach((el) => observer.observe(el));
-    
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.classList.add("show");
+      }, 250);
+    } else {
+        entry.target.classList.remove("show");
+    }
+  });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
+
 const brogoInteractive = document.querySelector("#interactiveBrogo img");
 setTimeout(() => {
   brogoInteractive.style.filter = "none";
